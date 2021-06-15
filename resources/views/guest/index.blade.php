de721f1ef0bbc9d742b76b280e325326f13dc3fb
Tommaso Borsatti <tommasoborsatti89@gmail.com>
2021-06-15 23:23:21 +0200

    Fix_2: index.blade.php - header.blace.php
    Effettuate alcune modifiche alla grafica della guest index; effettuate alcune modifiche all header.blade.php (come ad esempio il logo di ciboo). Aggiunto effetto hover alle card delle categorie. Tolto il colore di sfondo dal body e modificato il colore del layover nel variable-guest. Aggiunta una media query <900px width alle card delle categorie

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -6,53 +6,59 @@
 
 @section('contentGuest')
 <div class='mt-40' id="app">
+    
+    <div class="main_container">
+        <h1 class='main_title mt-20 mb-25'>La Selezione di CiBoo</h1>
+        <div class="mosaic_container">
+            <div class="category_card">
+                <img src="{{asset('storage/images/pizza.jpg')}}" alt="pizza">
+            </div>
+            <div class="category_card">
+                <img src="{{asset('storage/images/sushi.jpg')}}" alt="sushi">
+            </div>
+            <div class="category_card">
+                <img src="{{asset('storage/images/hamburger.jpg')}}" alt="hamburger">
+            </div>
+            <div class="category_card icecream">
+                <img src="{{asset('storage/images/gelati.jpg')}}" alt="gelati">
+            </div>
+            <div class="category_card">
+                <img src="{{asset('storage/images/cinese.jpg')}}" alt="cinese">
+            </div>
+            <div class="category_card">
+                <img src="{{asset('storage/images/messicano.jpg')}}" alt="messicano">
+            </div>
+        </div>
 
-    <div class="mosaic_container">
-        <div class="category_card">
-            <img src="{{asset('storage/images/pizza.jpg')}}" alt="gelati">
-        </div>
-        <div class="category_card">
-            <img src="{{asset('storage/images/sushi.jpg')}}" alt="gelati">
-        </div>
-        <div class="category_card">
-            <p>hamburger</p>
-        </div>
-        <div class="category_card icecream">
-            <img src="{{asset('storage/images/gelati.jpg')}}" alt="gelati">
-        </div>
-        <div class="category_card">
-            <p>cinese</p>
-        </div>
-        <div class="category_card">
-            <p>messicano</p>
-        </div>
+        <!--SELECT DI VUE-->
+        <select class="mt-40" v-model="category" v-on:change="categoriesSearch">
+            <option value="">Scegli la categoria</option>
+            @foreach ($categories as $category)
+                
+                <option  v-bind:value="{{ $category->id }}">
+                    {{ $category->name }}
+                </option>
+                
+            @endforeach
+        </select>
+
+        <ul v-if="category != ''">
+            <li v-for="restaurant in restaurants">
+                @{{ restaurant.restaurant }}
+            </li>
+        </ul> 
+
+        <ul v-else>
+            @foreach ($users as $user)
+                <li>
+                    {{ $user->restaurant }}
+                </li>
+            @endforeach
+        </ul>
+
     </div>
 
-    <!--SELECT DI VUE-->
-    <select class="mt-40" v-model="category" v-on:change="categoriesSearch">
-        <option value="">Scegli la categoria</option>
-        @foreach ($categories as $category)
-            
-            <option  v-bind:value="{{ $category->id }}">
-                {{ $category->name }}
-            </option>
-            
-        @endforeach
-    </select>
-
-    <ul v-if="category != ''">
-        <li v-for="restaurant in restaurants">
-            @{{ restaurant.restaurant }}
-        </li>
-    </ul> 
-
-    <ul v-else>
-        @foreach ($users as $user)
-            <li>
-                {{ $user->restaurant }}
-            </li>
-        @endforeach
-    </ul>
+    
 </div>   
 
 <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
--- resources/views/parts/guest/header.blade.php
+++ resources/views/parts/guest/header.blade.php
@@ -3,17 +3,17 @@
     <div id="navbarSupportedContent">
         <nav>
             <div class="container flex">
-                <img class="nav_logo" src="{{asset('storage/images/logo-deliveboo.jpeg')}}" alt="logo-deliveboo">
+                <img class="nav_logo" src="{{asset('storage/images/ciBoo.png')}}" alt="logo-deliveboo">
                 <!-- Right Side Of Navbar -->
                 <ul class="nav_login flex">
                     <!-- Authentication Links -->
                     @guest
                         <li class="mr-15">
-                            <a class="" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            <a class="nav_link" href="{{ route('login') }}">{{ __('Login') }}</a>
                         </li>
                         @if (Route::has('register'))
                             <li class="">
-                                <a class="" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                <a class="nav_link" href="{{ route('register') }}">{{ __('Register') }}</a>
                             </li>
                         @endif
                     @else
