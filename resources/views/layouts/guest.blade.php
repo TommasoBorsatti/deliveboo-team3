655a0777f9cf08888bdc974fa6a1f57337eb9a9f
Tommaso Borsatti <tommasoborsatti89@gmail.com>
2021-06-15 10:49:45 +0200

    Add: Struttura Base SCSS
    Abbiamo diviso il css in due file App e Guest, rispettando le divisioni Admin e Guest.
    Abbiamo diviso layouts, parts e partials allo stesso modo. 
    Abbiamo iniziato a compilare il file _variables-guest con alcune variabili che ci saranno utili nella parte frontend del Guest.

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,18 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('page_title')</title>
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js" integrity="sha512-bZS47S7sPOxkjU/4Bt0zrhEtWx0y0CRkhEp8IckzK+ltifIIE9EMIMTuT/mEzoIMewUINruDBIR/jJnbguonqQ==" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="{{asset('css/guest.css')}}">
 </head>
 <body>
 
-    @include('parts.admin.header')
+    @include('parts.guest.header')
 
     <main>
         @yield('content')
     </main>
 
-    @include('parts.admin.footer')
+    @include('parts.guest.footer')
     
 </body>
 </html>
