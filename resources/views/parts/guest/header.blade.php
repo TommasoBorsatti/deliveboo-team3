6d850ec1d87e333654c2d5f10e9f2c2b08734f23
Beatrice Castagno <beatrice.castagno1@gmail.com>
2021-06-15 18:11:33 +0200

    Add: header.blade.php e _header.scss
    Iniziata la struttura header della guest/index

--- resources/views/parts/guest/header.blade.php
+++ resources/views/parts/guest/header.blade.php
@@ -1,42 +1,50 @@
-<header>
-    <nav>
-        <nav class="">
-            <div class="">
-                <div class="" id="navbarSupportedContent">
-                    <!-- Right Side Of Navbar -->
-                    <ul>
-                        <!-- Authentication Links -->
-                        @guest
+<header class="">
+        
+    <div id="navbarSupportedContent">
+        <nav>
+            <div class="container flex">
+                <img class="nav-logo" src="{{asset('storage/images/logo-deliveboo.png')}}" alt="logo-deliveboo">
+                <!-- Right Side Of Navbar -->
+                <ul class="nav_login flex">
+                    <!-- Authentication Links -->
+                    @guest
+                        <li class="mr-15">
+                            <a class="" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @if (Route::has('register'))
                             <li class="">
-                                <a class="" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a class="" href="{{ route('register') }}">{{ __('Register') }}</a>
                             </li>
-                            @if (Route::has('register'))
-                                <li class="">
-                                    <a class="" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="">
-                                <a id="navbarDropdown" class="" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
+                        @endif
+                    @else
+                        <li class="">
+                            <a id="navbarDropdown" class="" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }}
+                            </a>
+                            <div class="" aria-labelledby="navbarDropdown">
+                                <a class="" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
                                 </a>
 
-                                <div class="" aria-labelledby="navbarDropdown">
-                                    <a class="" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @endguest
+                </ul>
             </div>
         </nav>
-    </nav>
+    </div>
+
+    <div class="jumbotron">
+        <div class="box-video">
+            <video controls>
+                <source src="video.mp4" type="video/mp4">
+            </video>
+        </div>
+    </div>
+        
 </header>
