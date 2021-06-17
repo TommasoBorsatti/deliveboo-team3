ec44074f69f59ad2576bf011e116a9982373c652
Tommaso Borsatti <tommasoborsatti89@gmail.com>
2021-06-17 11:37:19 +0200

    Fix:Routes
    Abbiamo aggiunto il link per tornare all'home page dalla sezione admin; Abbiamo implementato redirect alla dashboard del ristorante, all'avvenuto login

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/restaurants';
+    public const HOME = '/admin/plate';
 
     /**
      * Define your route model bindings, pattern filters, etc.
