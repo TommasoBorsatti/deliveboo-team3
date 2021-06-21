7ff6710ce1b347cd53d4af8c54527c285bf9997b
Tommaso Borsatti <tommasoborsatti89@gmail.com>
2021-06-21 18:01:01 +0200

    _Fix Guest/header-nojumbo.blade.php
    Adesso la nav bar delle pagine guest non rimanda più a login ma ha un link per tornare alla home page

--- resources/views/parts/guest/header-nojumbo.blade.php
+++ resources/views/parts/guest/header-nojumbo.blade.php
@@ -3,38 +3,8 @@
     <div id="navbarSupportedContent">
         <nav>
             <div class="container flex">
-                <a class="nav_logo"  href="{{route('search')}}"><img  src="{{asset('storage/images/ciBoo.png')}}" alt="logo-deliveboo"></a>  
-                <!-- Right Side Of Navbar -->
-                <ul class="nav_login flex">
-                    <!-- Authentication Links -->
-                    @guest
-                        <li class="mr-15">
-                            <a class="nav_link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                        @if (Route::has('register'))
-                            <li class="flex">
-                                <a class="nav_link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
-                        @endif
-                    @else
-                        <li class="flex">
-                            <a id="navbarDropdown" class="mr-15" href="{{route('admin.plate.index')}}" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
-                            </a>
-                            <div class="flex" aria-labelledby="navbarDropdown">
-                                <a class="nav_link" href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();
-                                                    document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                    @endguest
-                </ul>
+                <a class="nav_logo"  href="{{route('search')}}"><img  src="{{asset('storage/images/ciBoo.png')}}" alt="logo-deliveboo"></a>
+                <a class="nav_logo flex"  href="{{route('search')}}"> <i class="fas fa-home"></i> </a>
             </div>
         </nav>
     </div>
