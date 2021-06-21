c5dbb4a46d0a36ac1eb0014e5bb8bdfe7ddd46b5
Daniele <bambudio@libero.it>
2021-06-21 18:37:16 +0200

    Add: view admin/orders & statistiche

---
+++ app/Http/Controllers/Api/OrdersController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Order;
+use App\Plate;
+use Illuminate\Http\Request;
+
+class OrdersController extends Controller
+{
+    public function getOrders(Request $request)
+    {
+        $plates = Plate::where('user_id', $request->id)
+        ->get();
+        $orders = Order::all();
+        
+        $ordersFind = collect();
+        //Associazione dei Types ai Plates
+        foreach ($orders as $order) {
+            $order['plates'] = $order->plates;
+            foreach ($order['plates'] as $plate) {
+                
+                if ($plate->user_id == $request->id) {
+                   $ordersFind->add($order);
+                }
+            }
+        }
+        
+        return response()->json($ordersFind);
+    }
+}
--- app/Http/Controllers/User/StatsController.php
+++ app/Http/Controllers/User/StatsController.php
@@ -4,8 +4,13 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class StatsController extends Controller
 {
-    //
+    public function orders($id)
+    {   
+        $user = Auth::user();
+        return view('admin.orders', compact('user'));
+    }
 }
