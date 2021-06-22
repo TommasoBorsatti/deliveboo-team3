537cce2728b953c1e791d20ad0eae34481869b54
Daniele <bambudio@libero.it>
2021-06-22 11:56:29 +0200

    Fix: link alla dashboard

--- resources/views/parts/admin/header.blade.php
+++ resources/views/parts/admin/header.blade.php
@@ -19,7 +19,7 @@
                         @endif
                     @else
                         <li class="flex">
-                            <a id="navbarDropdown" class="mr-15" href="{{route('admin.plate.index')}}" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            <a id="navbarDropdown" class="mr-15" href="{{route('admin.plate.index')}}" aria-haspopup="true" aria-expanded="false" v-pre>
                                 {{ Auth::user()->name }}
                             </a>
                             <a id="navbarDropdown" class="mr-15" href="{{route('admin.restaurant.orders', Auth::user()->id)}}" >
